2b3e6ea991cf4ac8df41175b680626f0858dce93
andrelpoliveira <andreluiz.pradodeoliveira@gmail.com>
2023-03-11 16:33:48 -0300

    First Upload

---
+++ kookie/resources/js/Pages/Welcome.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import KookieLayout from '@/Layouts/KookieLayout.vue'
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <KookieLayout>
+        <div class="bg-red-900">
+            Welcome
+        </div>
+    </KookieLayout>
+   
+</template>
+
+
